d4be3667b9eaf24d81cc0699ebc5bd16872b2810
sushama <you@example.com>
2022-02-10 17:26:18 +0000

    First Commit Exercise02 Test

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -2,8 +2,25 @@ package com.techreturners.exercise002
 
 class Exercise002 {
 
-  def isFromManchester(/*person: Person*/): Boolean = false
+  def isFromManchester(person: Person): Boolean = {
+    if(person.city=="Manchester"){
+      true
+    }
+    else {
+      false
+    }
 
-  def canWatchFilm(/*person: Person, ageLimit: Int */): Boolean = false
+
+  }
+
+
+  def canWatchFilm(person: Person, ageLimit: Int): Boolean = {
+    if(person.age>18){
+      true
+    }
+    else {
+      false
+    }
+  }
 
 }
